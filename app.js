06393fd0c558aeb09c4ed1e154d27e31ebb4353b
fedsanchez93 <fed.sanchez@outlook.com>
2022-11-01 20:06:38 -0300

    Se agrego la variable de entorno

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 3030;
+const PORT = process.env.PORT || 3030
 
 app.use(express.static('public')); // Recursos estaticos
 
