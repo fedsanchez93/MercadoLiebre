a65ead357097613f9921a8259b589688b6ba12f5
fedsanchez93 <109318764+fedsanchez93@users.noreply.github.com>
2022-11-01 19:47:53 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const PORT = 3030;
+
+app.use(express.static('public')); // Recursos estaticos
+
+app.listen(PORT,() => console.log("Server listening on port  " + PORT));
+
+app.get("/", (req,res) => { 
+    res.sendFile(path.resolve("./views/home.html"));
+});
+
+app.get("/register", (req,res) => { 
+    res.sendFile(path.resolve("./views/register.html"));
+});
+
+app.get("/login", (req,res) => { 
+    res.sendFile(path.resolve("./views/login.html"));
+});
