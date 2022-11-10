892ecbd2d491f3f9ffbeda8486bd24bb0e8a9a7f
fedsanchez93 <fed.sanchez@outlook.com>
2022-11-10 20:04:18 -0300

    sse installó el paquete ejs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
 const mainRouter = require('./routers/mainRouter');
+const path = require('path');
 
 const PORT = process.env.PORT || 3030
 
 //Middleware
 app.use(express.static('public')); // Recursos estaticos
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine','ejs');
 
 app.listen(PORT,() => console.log("Server listening on port  " + PORT));
 
